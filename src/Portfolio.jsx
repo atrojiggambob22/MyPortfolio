6fad9cf48bec82a1743087aa89b7ad86a9264c04
Atrojiggambob <mathiasonyebuchi13@gmail.com>
2025-06-04 20:25:09 +0100

    My first portfolio push

---
+++ src/Portfolio.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Portfolio.css';
+// import './index.css'
+
+
+
+const Portfolio =()=>{
+    return(
+        <>  
+        <div>
+            <div className="d-grid">
+                <div>
+                    <h3>Web Research</h3>
+                    <h3>Software Dev</h3>
+                </div>
+                <div>
+                    <h3>I'm Mathias Christopher a Web Developer Based in Nigeria</h3>
+                </div>
+                <div>
+                    <h3>Web Developer</h3>
+                    <h3>Front-End Dev</h3>
+                </div>
+            </div>  
+                <div>
+                    <h3>Currently, I'm a freelance Web Developer and Co-Founder of Devrift Interprise Where we bring the Demands of Costomers and Business owners Website to LIFE and online Promotion</h3>
+                </div>
+        </div>
+        </>
+    )
+}
+export default Portfolio
