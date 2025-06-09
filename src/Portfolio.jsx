56b408bb31b2feda7d395afc8ff675bff4919019
Atrojiggambob <mathiasonyebuchi13@gmail.com>
2025-06-10 00:41:38 +0100

    fresh

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -225,15 +225,15 @@ const Portfolio =()=>{
                         <p>Finally,meet the young man behind the pixels a quick peek into my world</p>
                     </div>
 
-                    <div className="flex gap-20 my-[40px] overflow-hidden  ">
+                    <div className="flex gap-20 my-[40px] overflow-hidden underthecanopy ">
                         <div className="kings-ofmin gap-20  overflow-hidden relative top-2.5 ">
-                           <div className="flex gap-5 relative  left-[13px] top-1">
+                           <div className="flex gap-5 relative  left-[13px] top-1 bossofiggdfed">
                              <img src={me} alt="" className="miningforme  mx-[20px] relative left-2" />
                             <img src={enugu} alt="" className="miningforme2  mx-[20px] relative left-[-12px]" />
                            </div>
                             <img src={bayelsa} alt="" className="miningforme4" />
                         </div>
-                        <div className="w-[50%]"> 
+                        <div className="w-[50%] parentofthemp"> 
                             <p className="my-[20px]">I'm a passionate Web Developer from Enugu State, Nigeria. I was Born in Bayelsa State, from an early stage i knew how phones worked and operated them really well many were facinted by my skill.
                            </p>
 
@@ -244,11 +244,11 @@ const Portfolio =()=>{
                     </div>
 
                     <div>
-                        <div className="flex  gap-[40px]">
-                             <p className="w-[60%]">Creativity has always been my escape -first through drawing, now through designe and developement. I find joy through discovering hidden food ,spots and mobile gaming
+                        <div className="flex  gap-[40px] subparentofthemp ">
+                             <p className="w-[60%] sobunder">Creativity has always been my escape -first through drawing, now through designe and developement. I find joy through discovering hidden food ,spots and mobile gaming
                            </p>
 
-                           <p className="w-[30%]">I Love new places,Peaople,new conversation and the stories they bring
+                           <p className="w-[30%] sobunder ">I Love new places,Peaople,new conversation and the stories they bring
 
                            </p>
                         </div>
